435a6a701a957d2ddd22cf0b7035d14bac2b90a7
Nicolai Wagner <nocwagner@gmail.com>
2024-04-16 14:22:20 +0200

    Controls added to the style section for Darkmode widget

--- widgets/darkmode/darkmode-widget.js
+++ widgets/darkmode/darkmode-widget.js
@@ -1,10 +1,21 @@
-document
-  .getElementById("dark-mode-toggle")
-  .addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode-bg");
-    document
-      .querySelectorAll("h1, h2, h3, h4, h5, h6")
-      .forEach(function (element) {
-        element.classList.toggle("dark-mode-header");
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("dark-mode-toggle")
+    .addEventListener("click", function () {
+      document.body.classList.toggle("dark-mode-bg");
+      document
+        .querySelectorAll("h1, h2, h3, h4, h5, h6")
+        .forEach(function (element) {
+          element.classList.toggle("dark-mode-header");
+        });
+      document.querySelectorAll("p").forEach(function (element) {
+        element.classList.toggle("dark-mode-paragraph");
       });
-  });
+      document.querySelectorAll("a").forEach(function (element) {
+        element.classList.toggle("dark-mode-link");
+      });
+      document.querySelectorAll("span").forEach(function (element) {
+        element.classList.toggle("dark-mode-span");
+      });
+    });
+});
--- widgets/darkmode/darkmode-widget.php
+++ widgets/darkmode/darkmode-widget.php
@@ -9,7 +9,7 @@ class Darkmode_Widget extends Elementor\Widget_Base {
     }
 
     public function get_title() {
-        return 'Dark Mode Toggle';
+        return 'Aveo WA - Dark Mode Toggle';
     }
 
     public function get_icon() {
@@ -22,34 +22,69 @@ class Darkmode_Widget extends Elementor\Widget_Base {
     }
 
     protected function _register_controls() {
+        //Start content tab
         $this->start_controls_section(
             'section_content',
             [
-                'label' => __( 'Content', 'plugin-name' ),
+                'label' => __( 'Content', 'aveo-access' ),
+                //'tab' => Controls_Manager::TAB_CONTENT,
             ]
         );
-
+        
+        // Toggle type icon/button
         $this->add_control(
             'toggle_type',
             [
-                'label' => __( 'Toggle Type', 'plugin-name' ),
+                'label' => __( 'Toggle Type', 'aveo-access' ),
                 'type' => Controls_Manager::SELECT,
                 'options' => [
-                    'button' => __( 'Button', 'plugin-name' ),
-                    'icon' => __( 'Icon', 'plugin-name' ),
+                    'button' => __( 'Button', 'aveo-access' ),
+                    'icon' => __( 'Icon', 'aveo-access' ),
                 ],
                 'default' => 'button',
-                'description' => __( 'Choose between a button or an icon for the dark mode toggle.', 'plugin-name' ),
+                'description' => __( 'Choose between a button or an icon for the dark mode toggle.', 'aveo-access' ),
             ]
         );
 
+        // Button background color light mode 
+        $this->add_control(
+            'button_background_color_light',
+            [
+                'label' => __( 'Button Background Color Light Mode', 'aveo-access' ),
+                'type' => Controls_Manager::COLOR,
+                'default' => '#000000',
+                'description' => __( 'Choose the background color of the button for the light mode.', 'aveo-access' ),
+                'condition' => [
+                    'toggle_type' => 'button',
+                ],
+            ]
+        );
+
+        // Button text field
         $this->add_control(
             'button_text',
             [
-                'label' => __( 'Button Text', 'plugin-name' ),
+                'label' => __( 'Button Text', 'aveo-access' ),
                 'type' => Controls_Manager::TEXT,
-                'default' => __( 'Toggle Dark Mode', 'plugin-name' ),
-                'placeholder' => __( 'Enter button text here', 'plugin-name' ),
+                'default' => __( 'Toggle Dark Mode', 'aveo-access' ),
+                'placeholder' => __( 'Enter button text here', 'aveo-access' ),
+                'condition' => [
+                    'toggle_type' => 'button',
+                ],
+            ]
+        );
+        // Button size control
+        $this->add_control(
+            'button_size',
+            [
+                'label' => __( 'Button Size', 'aveo-access' ),
+                'type' => Controls_Manager::SELECT,
+                'options' => [
+                    'large' => __( 'Large', 'aveo-access' ),
+                   'medium' => __( 'Medium', 'aveo-access' ),
+                   'small' => __( 'Small', 'aveo-access' ),
+                ],
+                'default' =>'medium',
                 'condition' => [
                     'toggle_type' => 'button',
                 ],
@@ -60,7 +95,7 @@ class Darkmode_Widget extends Elementor\Widget_Base {
         $this->add_control(
             'dark_mode_bg_color',
             [
-                'label' => __( 'Dark Mode Background Color', 'plugin-name' ),
+                'label' => __( 'Dark Mode Background Color', 'aveo-access' ),
                 'type' => \Elementor\Controls_Manager::COLOR,
                 'selectors' => [
                     'background-color: {{VALUE}}; color: white;',
@@ -72,24 +107,173 @@ class Darkmode_Widget extends Elementor\Widget_Base {
         $this->add_control(
             'dark_mode_header_color',
             [
-                'label' => __( 'Dark Mode Header Text Color', 'plugin-name' ),
+                'label' => __( 'Dark Mode Header Text Color', 'aveo-access' ),
                 'type' => \Elementor\Controls_Manager::COLOR,
                 'selectors' => [
-                    '{{WRAPPER}} .dark-mode-header, 
-                    {{WRAPPER}} .dark-mode-header h1, 
-                    {{WRAPPER}} .dark-mode-header h2, 
-                    {{WRAPPER}} .dark-mode-header h3, 
-                    {{WRAPPER}} .dark-mode-header h4, 
-                    {{WRAPPER}} .dark-mode-header h5, 
-                    {{WRAPPER}} .dark-mode-header h6' => 'color: {{VALUE}};',
+                    'color: {{VALUE}};',
                 ],
             ]
         );
 
+        //Color picker for paragraph text
+        $this->add_control(
+            'dark_mode_paragraph_color',
+            [
+                'label' => __( 'Dark Mode Paragraph Text Color', 'aveo-access' ),
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'selectors' => [
+                    'color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        //Color picker for a tags
+        $this->add_control(
+            'dark_mode_a_color',
+            [
+                'label' => __( 'Dark Mode A Tag Color', 'aveo-access' ),
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'selectors' => [
+                    'color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        // Color picker for span tags 
+        $this->add_control(
+            'dark_mode_span_color',
+            [
+                'label' => __( 'Dark Mode Span Tag Color', 'aveo-access' ),
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'selectors' => [
+                    'color: {{VALUE}};',
+                ],
+            ]
+        ); 
+
+        
+   
+        // END: Content tab
+        $this->end_controls_section();
     
 
+    // START: Style tab
+    $this->start_controls_section(
+        'section_style',
+        [
+            'label' => __( 'Style', 'aveo-access' ),
+            'tab' => Controls_Manager::TAB_STYLE,
+        ]
+    );
+            // Control typography for the button
+        $this->add_group_control(
+            \Elementor\Group_Control_Typography::get_type(),
+            [
+                'name' => 'button_typography',
+                'label' => __( 'Button Typography', 'aveo-access' ),
+                'selector' => '{{WRAPPER}} #dark-mode-toggle',
+                'condition' => [
+                    'toggle_type' => 'button',
+                ],
+            ]
+        );
+
+        // Control text color for the button
+        $this->add_control(
+            'button_text_color',
+            [
+                'label' => __( 'Button Text Color', 'aveo-access' ),
+                'type' => Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} #dark-mode-toggle' => 'color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        // Control background color for the button
+        $this->add_control(
+            'button_background_color',
+            [
+                'label' => __( 'Button Background Color', 'aveo-access' ),
+                'type' => Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} #dark-mode-toggle' => 'background-color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        // Control border style for the button
+        $this->add_control(
+            'button_border_type',
+            [
+                'label' => __( 'Button Border Type', 'aveo-access' ),
+                'type' => Controls_Manager::SELECT,
+                'options' => [
+                    'none' => __( 'None', 'aveo-access' ),
+                    'solid' => __( 'Solid', 'aveo-access' ),
+                    'dotted' => __( 'Dotted', 'aveo-access' ),
+                    'dashed' => __( 'Dashed', 'aveo-access' ),
+                    'double' => __( 'Double', 'aveo-access' ),
+                    'groove' => __( 'Groove', 'aveo-access' ),
+                    'ridge' => __( 'Ridge', 'aveo-access' ),
+                    'inset' => __( 'Inset', 'aveo-access' ),
+                    'outset' => __( 'Outset', 'aveo-access' ),
+                ],
+                'default' => 'none',
+                'selectors' => [
+                    '{{WRAPPER}} #dark-mode-toggle' => 'border-style: {{VALUE}};',
+                ],
+            ]
+        );
+
+        // Control border radius for the button
+        $this->add_control(
+            'button_border_radius',
+            [
+                'label' => __( 'Button Border Radius', 'aveo-access' ),
+                'type' => Controls_Manager::DIMENSIONS,
+                'size_units' => [ 'px', '%', 'em' ],
+                'selectors' => [
+                    '{{WRAPPER}} #dark-mode-toggle' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        // Control box shadows for the button
+        $this->add_group_control(
+            \Elementor\Group_Control_Box_Shadow::get_type(),
+            [
+                'name' => 'button_box_shadow',
+                'label' => __( 'Button Box Shadows', 'aveo-access' ),
+                'selector' => '{{WRAPPER}} #dark-mode-toggle',
+            ]
+        );
+
+        // Control padding (spacing) for the button
+        $this->add_responsive_control(
+            'button_spacing',
+            [
+                'label' => __( 'Button Spacing', 'aveo-access' ),
+                'type' => Controls_Manager::DIMENSIONS,
+                'size_units' => [ 'px', '%', 'em' ],
+                'selectors' => [
+                    '{{WRAPPER}} #dark-mode-toggle' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
+            ]
+        );
+
+
+            
+        // END: Style tab
         $this->end_controls_section();
-    }
+
+        }                  
+                
+    
+
+
+
+
 
     protected function render() {
         $settings = $this->get_settings_for_display();
@@ -108,15 +292,36 @@ class Darkmode_Widget extends Elementor\Widget_Base {
             echo '
                 .dark-mode-bg, .dark-mode-bg div, .dark-mode-bg header, .dark-mode-bg section, .dark-mode-bg article, .dark-mode-bg body, dark-mode-bg #e-con-inner {
                     background-color: ' . $settings['dark_mode_bg_color'] . ';
-                    color: white; /* Ensure text readability */
+                    
                 }
             ';
         }
 
         if (!empty($settings['dark_mode_header_color'])) {
             echo '
-                .dark-mode-header, .dark-mode-header h1, .dark-mode-header h2, .dark-mode-header h3, .dark-mode-header h4, .dark-mode-header h5, .dark-mode-header h6 {
-                    color: ' . $settings['dark_mode_header_color'] . ';
+                h1.dark-mode-header, h2.dark-mode-header, h3.dark-mode-header, h4.dark-mode-header, h5.dark-mode-header, h6.dark-mode-header {
+                    color: ' . $settings['dark_mode_header_color'] . ' !important;
+                }
+            ';
+        }
+        if (!empty($settings['dark_mode_paragraph_color'])) {
+            echo '
+                p.dark-mode-paragraph, span.dark-mode-paragraph, a.dark-mode-paragraph {
+                    color: ' . $settings['dark_mode_paragraph_color'] . ' !important;
+                }
+            ';
+        }
+        if (!empty($settings['dark_mode_a_color'])) {
+            echo '
+                a.dark-mode-link  {
+                    color: ' . $settings['dark_mode_a_color'] . ' !important;
+                }
+            ';
+        }
+        if (!empty($settings['dark_mode_span_color'])) {
+            echo '
+                span.dark-mode-span {
+                    color: ' . $settings['dark_mode_span_color'] . ' !important;
                 }
             ';
         }
